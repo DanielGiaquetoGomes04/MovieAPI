3fab25e74a1f8ca96b51a659a3e3e955d2e47fd6
Daniel Giaqueto <daniel_giaqueto_gomes@outlook.com>
2022-08-25 10:53:14 -0300

    API quase completa

---
+++ FilmesAPI/Controllers/FilmeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FilmesAPI.Models;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class FilmeController : ControllerBase
+    {
+        private static List<Filme> filmes = new List<Filme>();
+        private static int id = 1;
+
+        [HttpPost] // responsável por criar um recurso novo no sistema
+        public IActionResult AdicionaFilme([FromBody]Filme filme)
+        {
+            filme.Id = id++;
+            filmes.Add(filme);
+            // mostra o status da requisição e onde foi criado
+            return CreatedAtAction(nameof(RecuperaFilmePorId), new { Id = filme.Id }, filme);
+        }
+       
+        [HttpGet]
+        // Retorna uma lista de filmes
+        public IActionResult RecuperaFilmes()
+        {
+            return Ok(filmes);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult RecuperaFilmePorId(int id)
+        {
+            // retorne o elemento de nossa lista que possui o id correspondente.
+            Filme filme = filmes.FirstOrDefault(filme => filme.Id == id);
+            if (filme != null)
+            {
+                return Ok(filme);
+            }
+            // retorna erro 404
+            return NotFound();
+            /*
+            foreach(Filme filme in filmes) {
+                if (filme.Id == id)
+                {
+                    return filme;
+                }
+            }
+            return null;
+            */
+        }
+    }
+}
